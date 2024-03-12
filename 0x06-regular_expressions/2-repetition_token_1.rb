d90a0596bf646efedc50ed91a49deb41a2ebbfad
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-03-12 23:43:45 +0800

    Commting

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/env ruby
-result = ARGV[0].scan(/hb*tn/)
-for x in result do
-    print x
-end
-puts
+puts ARGV[0].scan(/hb*tn/)
--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/env ruby
-result = ARGV[0].scan(/hbt*n/)
-for x in result do
-    print x
-end
-puts
+puts ARGV[0].scan(/hbt*n/)
