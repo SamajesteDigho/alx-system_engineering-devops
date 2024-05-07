9cbfcf3b03bec54f2dc901a9a1f9ca1145fdd2a5
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-08 06:18:29 +0800

    Exo 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # create a package installation manifest
 package { 'flask':
-    ensure   => '2.1.0',
+    ensure   => 'v2.1.0',
     provider => 'pip3',
-    path     => [ '/usr/bin/python3', '/bin/' ],
 }
