daabb585b42785aa8298f7084c139e6f017f3cdf
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-08 09:02:36 +0800

    Commiting

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,16 @@
 # Configures the /etc/ssh/ssh_config file
+ini_setting { 'disable password_auth':
+    ensure  => present,
+    path    => '/etc/ssh/ssh_config',
+    section => 'Host',
+    setting => 'PasswordAuthentication',
+    value   => 'no',
+},
+
+ini_setting { 'disable password_auth':
+    ensure  => present,
+    path    => '/etc/ssh/ssh_config',
+    section => 'Host',
+    setting => 'IdentityFile',
+    value   => '~/.ssh/school',
+},
