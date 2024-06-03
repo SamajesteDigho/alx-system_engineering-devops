10091d8a921c2ed8443f36277f398e531457cba0
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-04 07:58:12 +0800

    Exo 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -26,9 +26,21 @@ if __name__ == "__main__":
         users = response1.json()
 
         for user in users:
-            url2 = url2.format(user["id"])
-            response2 = requests.get(url=url2)
+            tdu = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+                user["id"])
+            response2 = requests.get(url=tdu)
             todos = response2.json()
+
+            completed = []
+            for x in todos:
+                if x['completed'] is True:
+                    completed.append(x)
+
+            print("Employee {} is done with tasks({}/{}):".
+                  format(user['name'], len(completed), len(todos)))
+            for x in completed:
+                print("\t {}".format(x['title']))
+
             data["{}".format(user['id'])] = parse_format_data(user, todos)
 
         with open('todo_all_employees.json', 'w') as file:
