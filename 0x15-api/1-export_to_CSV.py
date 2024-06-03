9527b5d90c6923fdf9bcc919c87b189e49efbedf
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-04 07:12:47 +0800

    Exo 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,3 +2,38 @@
 """
     Here the code description and module
 """
+import requests
+import sys
+
+
+args = sys.argv
+if __name__ == "__main__":
+    if len(args) > 0:
+        id = args[1]
+        url1 = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+        url2 = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+        try:
+            response1 = requests.get(url=url1)
+            user = response1.json()
+            response2 = requests.get(url=url2)
+            todos = response2.json()
+
+            completed = []
+            for x in todos:
+                if x['completed'] is True:
+                    completed.append(x)
+
+            print("Employee {} is done with tasks({}/{}):".
+                  format(user['name'], len(completed), len(todos)))
+            for x in completed:
+                print("\t {}".format(x['title']))
+
+            with open('{}.csv'.format(id), 'w') as file:
+                lines = []
+                for x in todos:
+                    string = '"{}","{}","{}","{}"\n'.format(
+                        id, user['username'], x['completed'], x['title'])
+                    lines.append(string)
+                file.writelines(lines)
+        except Exception as e:
+            pass
