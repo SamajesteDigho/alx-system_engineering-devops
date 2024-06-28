2d864e1784a2e393b62802b0b8933c3715372c94
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-28 08:30:22 +0800

    Commiting

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Configure the trafic amount on Nginx
+
+# Increase the ULIMIT
+exec { 'fix--for-nginx',
+    command => '/bin/sed -i "s/15/2048/" /etc/default/nginx',
+    path    => 'usr/local/bin:/bin',
+}
+
+# Restart Nginx
+exec { 'nginx-restart',
+    command => '/etc/init.d/nginx restart',
+    path    => '/etc/init.d/',
+}
