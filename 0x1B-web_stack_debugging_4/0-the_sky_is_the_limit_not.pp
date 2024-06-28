0fc2b22ec17c93b584f511f18399e9d662f50d02
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-28 08:43:11 +0800

    Commting

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
 # Configure the trafic amount on Nginx
 
 # Increase the ULIMIT
-exec { 'fix--for-nginx',
+exec { 'fix--for-nginx':
     command => '/bin/sed -i "s/15/2048/" /etc/default/nginx',
     path    => 'usr/local/bin/:/bin/',
 }
 
 # Restart Nginx
-exec { 'nginx-restart',
+exec { 'nginx-restart':
     command => '/etc/init.d/nginx restart',
     path    => '/etc/init.d/',
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,13 @@
 # Enable helberton user to login and open files
 
 # Increase head file limit
-exec { 'increase-head-file',
+exec { 'increase-head-file':
     command => 'sed -i "/^holberton hard/s/5/50000/" /etc/security/limits.conf',
     path    => '/usr/local/bin/:/bin/',
 }
 
 # Increase soft file limit
-exec { 'increase-soft-file-limit',
+exec { 'increase-soft-file-limit':
     command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
     path    => '/usr/local/bin/:/bin/',
 }
