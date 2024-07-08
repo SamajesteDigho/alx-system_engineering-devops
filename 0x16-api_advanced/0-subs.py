5c57cbabf18004600d989a2adcf6603329cc0aa7
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-07-08 08:05:41 +0800

    Commiting

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,17 +7,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Get the number of subscribers in a subreddit """
-    bearer = "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNzE4ODQxOTA2LjA2Mjc3MSwiaWF0IjoxNzE4NzU1NTA2LjA2Mjc3MSwianRpIjoiT3dkd2NWY0kwWm1pY0xzaE5ZM0FOOEI4NzdwWE9BIiwiY2lkIjoiSmVZamhQeFlXMGNxUlBmaHJNLWhQdyIsImxpZCI6InQyXzEycnFqdDZ2OHQiLCJhaWQiOiJ0Ml8xMnJxanQ2djh0IiwibGNhIjoxNzE4NzQ3NjE4MzczLCJzY3AiOiJlSnlLVnRKU2lnVUVBQURfX3dOekFTYyIsImZsbyI6OX0.eGO23wQGQB0h8vK8AQQZbiysFMiHVETp3E6rmXG6uxmid3DOibNGRnwlhL9Kii25qVMRUurvgKWcoQjLeLNpQcFU8vuAIkBvZgYUxpjbzUtn6wKYPcNyQT2KzunzC81ArrC8BzX3FF7KLg3Ra__N4LTUbp5-MrT2kFBtOfjkE8AEMn0qlTOmjX0yV2MZP2JpfV5KBaNnkZvzvx7mb3OlrU5Z4afFavtC8wMTSP_H_VlUQfU41KWyFThvMO2U-X7nbU7SQDQoetNsJRsRFzRoVVSG2ZuyOHw5OZ4vu8kpxhuGrrpER0peh9lq8kgjYtvqN1mwIVDXR1j5Nl3qylL_SQ"
     headers = {
-        "Authorization": "bearer {}".format(bearer),
-        "User-Agent": "ALX_SE_Project/0.1 by SamajesteDigho"
+        "user-agent": "ALX_SE_Project/0.1 by SamajesteDigho"
     }
-    url = "https://oauth.reddit.com/r/{}/about".format(subreddit)
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        try:
-            return data['data']['subscribers']
-        except:
-            return 0
-    return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    data = response.json().get("data")
+    return data.get("subscribers", 0)
