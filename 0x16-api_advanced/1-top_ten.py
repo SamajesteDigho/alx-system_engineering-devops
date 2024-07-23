bfc6ab98cfeb9fd3599eb262a02a361f74ef1b63
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-07-23 11:04:10 +0800

    All

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,17 +9,14 @@ def top_ten(subreddit):
     """ Get the number of subscribers in a subreddit """
     headers = {
         # "Authorization": "bearer {}".format(bearer),
-        "User-Agent": "ALX_SE_Project/0.1 by SamajesteDigho"
+        "user-agent": "ALX_SE_Project/0.1 by SamajesteDigho"
     }
     url = "https://www.reddit.com/r/{}/hot?limit=10".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        try:
-            children = data['data']['children']
-            for child in children:
-                print(child['data']['title'])
-        except Exception as e:
-            print(None)
-    else:
+    if response.status_code != 200:
         print(None)
+    else:
+        data = response.json().get("data", {})
+        children = data.get('children', [])
+        for child in children:
+            print(child.get('data', {}).get('title'))
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,33 +5,29 @@
 import requests
 
 
-def recurse_merge(subreddit, hot_list=[], count=0, after=None):
+def recurse(subreddit, hot_list=[], after=None):
     """ Get the number of subscribers in a subreddit """
     headers = {
         # "Authorization": "bearer {}".format(bearer),
-        "User-Agent": "ALX_SE_Project/0.1 by SamajesteDigho"
+        "user-agent": "ALX_SE_Project/0.1 by SamajesteDigho"
     }
-    if after is None:
-        url = "https://www.reddit.com/r/{}/hot?limit=99&count={}".format(subreddit, count)
+    params = {"limit" : 100}
+    
+    url = "https://www.reddit.com/r/{}/hot".format(subreddit)
+    if after:
+        params['after'] = after
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    print("Here we go")
+    if response.status_code != 200:
+        return None
+    data = response.json().get('data', {})
+    children = data.get('children', [])
+    for child in children:
+        hot_list.append(child.get('title'))
+
+    after = data.get('after')
+    if after:
+        recurse(subreddit, hot_list=hot_list, after=after)
     else:
-        url = "https://www.reddit.com/r/{}/hot?limit=99&count={}&after{}".format(
-            subreddit, count, after)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        try:
-            children = data['data']['children']
-            after = data['data']['after']
-            count += int(data['data']['dist'])
-            for child in children:
-                hot_list.append(child['data']['title'])
-            if after is not None:
-                recurse_merge(subreddit, hot_list=hot_list, count=count, after=after)
-        except Exception:
-            return hot_list
-    return hot_list
-
-
-def recurse(subreddit, hot_list=[]):
-    """ Recursively """
-    return recurse_merge(subreddit, hot_list)
+        return hot_list
