0328b2d19d15693342cceaf75f79d27534809542
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-07-30 07:57:53 +0800

    Commiting

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Get the number of subscribers in a subreddit """
-    headers = {
-        # "Authorization": "bearer {}".format(bearer),
-        "user-agent": "ALX_SE_Project/0.1 by SamajesteDigho"
-    }
+    headers = {'user-agent': 'Mozilla/10.0/API'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
-    data = response.json().get("data")
-    return data.get("subscribers", 0)
+    data = response.json().get('data')
+    number = data.get('subscribers')
+    return number
